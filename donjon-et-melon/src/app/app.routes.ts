c8f5aec6a68ad26b6428945c11fa5e5b0d149bad
Roxane Le Tensorer <roxane.letensorer@gmail.com>
2024-10-06 22:14:28 +0200

    add routing

--- donjon-et-melon/src/app/app.routes.ts
+++ donjon-et-melon/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { DungeonComponent } from './dungeon/dungeon.component';
+import { CharacterComponent } from './character/character.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'donjons', component: DungeonComponent },
+  { path: 'mon-perso', component: CharacterComponent },
+];
